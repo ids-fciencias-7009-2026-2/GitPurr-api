a96e343a3cbbb367444c133bcce5671ce299acd3
Karina <karina_hf3@gmail.com>
2026-02-17 13:00:43 -0600

    commit

---
+++ src/main/kotlin/com/gitpurr/ids/sistemaAdopcion/SistemaAdopcionApplication.kt
@@ -0,0 +1,11 @@
+package com.gitpurr.ids.sistemaAdopcion
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SistemaAdopcionApplication
+
+fun main(args: Array<String>) {
+	runApplication<SistemaAdopcionApplication>(*args)
+}
---
+++ src/test/kotlin/com/gitpurr/ids/sistemaAdopcion/SistemaAdopcionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gitpurr.ids.sistemaAdopcion
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SistemaAdopcionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
