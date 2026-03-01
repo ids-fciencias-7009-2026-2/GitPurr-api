3b87522f0e639b5abf5a2b3abd60a7fcedc62d54
Brenda <brendarj1124@gmail.com>
2026-03-01 15:29:22 -0600

    application.kt modificado para que el proyecto detecte el .env

--- src/main/kotlin/com/gitpurr/ids/sistemaAdopcion/SistemaAdopcionApplication.kt
+++ src/main/kotlin/com/gitpurr/ids/sistemaAdopcion/SistemaAdopcionApplication.kt
@@ -2,10 +2,15 @@ package com.gitpurr.ids.sistemaAdopcion
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import io.github.cdimascio.dotenv.dotenv
 
 @SpringBootApplication
 class SistemaAdopcionApplication
 
 fun main(args: Array<String>) {
+	dotenv().entries().forEach {
+		System.setProperty(it.key, it.value)
+	}
 	runApplication<SistemaAdopcionApplication>(*args)
 }
+
