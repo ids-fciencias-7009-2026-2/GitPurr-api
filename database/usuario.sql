0e04e51ed136530ece75bf78127c518dc9d4cfad
SamCharlize <samantha_charlize_85@ciencias.unam.mx>
2026-03-02 10:26:03 -0600

    Práctica 2: Mi parte fue la creacion de la tabla

---
+++ database/usuario.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS usuario;
+
+CREATE TABLE usuario (
+                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                         nombre VARCHAR(150) NOT NULL,
+                         email VARCHAR(150) NOT NULL UNIQUE,
+                         password VARCHAR(255) NOT NULL,
+                         token VARCHAR(255)
+);
